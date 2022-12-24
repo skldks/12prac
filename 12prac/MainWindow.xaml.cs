8fbf011a8dbe7888f2fc7ed63ca09ae3c1df6bf1
Алёшус Прайм < @DESKTOP-NV07QGE>
2022-12-24 07:18:10 +0300

    Добавьте файлы проекта.

---
+++ 12prac/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
+using System.Windows.Threading;
+using ClassLibrary;
+using System.Windows.Diagnostics;
+
+namespace _12prac
+{
+    public partial class MainWindow : Window
+    {
+
+        rabotka rab = new rabotka();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private DispatcherTimer timer;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(this.Timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            time.Text = now.ToString("HH:mm:ss");
+            data.Text = now.ToString("dd.MM.yyyy");
+        }
+
+        private void Treugol_Click(object sender, RoutedEventArgs e)
+        {
+            if (X1.Text == "" || X2.Text == "" || X3.Text == "" || Y1.Text == "" || Y2.Text == "" || Y3.Text == "")
+            {
+                MessageBox.Show("Введите недостающие данные");
+            }
+            else
+            {
+                double perim; double plosh;
+                Double.TryParse(X1.Text, out double x1); Double.TryParse(X2.Text, out double x2); Double.TryParse(X3.Text, out double x3);
+                Double.TryParse(Y1.Text, out double y1); Double.TryParse(Y2.Text, out double y2); Double.TryParse(Y3.Text, out double y3);
+                rab.PerPloshTr(x1, y1, x2, y2, x3, y3, out perim, out plosh);
+                perim = Math.Round(perim, 3);
+                plosh = Math.Round(plosh, 3);
+                Perim.Text = $"{perim}";
+                Plosh.Text = $"{plosh}";
+            }
+            X1.Focus();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            X1.Clear(); X2.Clear(); X3.Clear(); Y1.Clear(); Y2.Clear(); Y3.Clear(); Plosh.Clear(); Perim.Clear();
+            Chislo.Clear(); Summa.Clear(); Proizv.Clear();
+        }
+
+        private void Chisl(object sender, RoutedEventArgs e)
+        {
+            if (Chislo.Text == "")
+            {
+                MessageBox.Show("Введите данные");
+            }
+            else
+            {
+                Int32.TryParse(Chislo.Text, out int chis);
+                if (chis < 99 || chis > 999)
+                {
+                    MessageBox.Show("Введите данные правильно");
+                    return;
+                }
+                else
+                {
+                    Summa.Text = $"{rab.ChisloSum(chis)}";
+                    Proizv.Text = $"{rab.ChisloProizv(chis)}";
+                }
+            }
+            Chislo.Focus();
+        }
+
+        private void Spavka(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Кузнецов Алексей Алексеевич ИСП-31. Вариант 13: Даны координаты трех вершин треугольника: (x1, y1), (x2, y2), (x3, y3). Найти его периметр и площадь, используя окнолу для расстояния между двумя точками на плоскости (см. задание 12). Для нахождения площади треугольника со сторонами a,b, c использовать окнолу Герона Дано трехзначное число. Найти сумму и произведение его цифр.");
+        }
+
+        private void Support(object sender, RoutedEventArgs e)
+        {
+            string target = "https://";
+            System.Diagnostics.Process.Start(target);
+        }
+
+        private void Quit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void Datachanged_Click(object sender, TextChangedEventArgs e)
+        {
+            Summa.Clear(); Proizv.Clear();
+        }
+
+        private void Text_Changed(object sender, TextChangedEventArgs e)
+        {
+            Perim.Clear(); Plosh.Clear();
+        }
+
+    }
+}
---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class rabotka
+    {
+        public void PerPloshTr(double x1, double y1, double x2, double y2, double x3, double y3, out double Perim, out double Plosh)
+        {
+            double stA = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double stB = Math.Sqrt(Math.Pow(x3 - x2, 2) + Math.Pow(y3 - y2, 2));
+            double stC = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+            Perim = stA + stB + stC;
+            double p = 0;
+            p = (stA + stB + stC) / 2;
+            Plosh = Math.Sqrt(p * (p - stA) * (p - stB) * (p - stC));
+        }
+
+        public int ChisloSum(int chis)
+        {
+            int a, b, c;
+            a = chis / 100;
+            b = (chis / 10) % 10;
+            c = chis % 10;
+            int summ = a + b + c;
+            return summ;
+        }
+        public int ChisloProizv(int chis)
+        {
+            int a, b, c;
+            a = chis / 100;
+            b = (chis / 10) % 10;
+            c = chis % 10;
+            int proizv = a * b * c;
+            return proizv;
+        }
+    }
+}
